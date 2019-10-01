53ce334e79088067f6080fc30bc6e265ff007b5a
Your Name <you@example.com>
2019-10-01 14:19:57 -0300

    Versão 1
    Primeiro commit da aplicação

---
+++ pjteste/src/dependencies.php
@@ -0,0 +1,42 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+    $container = $app->getContainer();
+
+    // view renderer
+    $container['renderer'] = function ($c) {
+        $settings = $c->get('settings')['renderer'];
+        return new \Slim\Views\PhpRenderer($settings['template_path']);
+    };
+
+    // monolog
+    $container['logger'] = function ($c) {
+        $settings = $c->get('settings')['logger'];
+        $logger = new \Monolog\Logger($settings['name']);
+        $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+        $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
+        return $logger;
+    };
+
+    $container['pdo'] = function ($c) {
+        // better load the settings with $container->get('settings')
+        $host = '127.0.0.1';
+        $dbname = 'sga2';
+        $username = 'root';
+        $password = '';
+        $charset = 'utf8';
+        $collate = 'utf8_unicode_ci';
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_PERSISTENT => false,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES $charset COLLATE $collate"
+        ];
+    
+        return new PDO($dsn, $username, $password, $options);
+    };
+};
