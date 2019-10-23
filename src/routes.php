b39c6704b34a4b1d67ee5ca42a913a6b4fff94c6
Your Name <you@example.com>
2019-10-23 16:40:02 -0300

    Apaguei o localizador
    cabouce

--- src/routes.php
+++ src/routes.php
@@ -9,11 +9,10 @@ return function (App $app) {
 
     $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
 
-      
-
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
 
+        //apaguei
 
         // Render index view
         return $container->get('renderer')->render($response, 'index.phtml', $args);
